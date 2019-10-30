87e95d7ae3dfb23e5b82f0c53c219fea2153c88f
Oliver Egger <oliver.egger@ahdis.ch>
2019-10-30 15:04:33 +0100

    Revert "snapshot gen diff for id/ext elem/attrib change"
    This reverts commit 476217299f1e1aedb6d4ad4135ef9ed28a2cc814.

--- org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/conformance/ProfileUtilities.java
+++ org.hl7.fhir.r5/src/main/java/org/hl7/fhir/r5/conformance/ProfileUtilities.java
@@ -919,19 +919,10 @@ public class ProfileUtilities extends TranslatingUtilities {
           outcome.setPath(fixedPathDest(contextPathDst, outcome.getPath(), redirector, contextPathSrc));
           if (res == null)
             res = outcome;
-          
-          if (StructureDefinition.StructureDefinitionKind.LOGICAL.equals(destKind) && (!diffMatches.get(0).hasRepresentation() || diffMatches.get(0).hasRepresentation(PropertyRepresentation.XMLTEXT)) 
-              && outcome.getId()!=null && outcome.getId().endsWith("id")) {
-            outcome.getRepresentation().clear();
-            outcome.setBase(null);
-            outcome.setMin(diffMatches.get(0).getMin());
-            outcome.setMax(diffMatches.get(0).getMax());
-          } else {
-            updateFromBase(outcome, currentBase);
-          }
+          updateFromBase(outcome, currentBase);
           if (diffMatches.get(0).hasSliceName())
             outcome.setSliceName(diffMatches.get(0).getSliceName());
-          updateFromDefinition(outcome, diffMatches.get(0), profileName, trimDifferential, url, srcSD, destKind);
+          updateFromDefinition(outcome, diffMatches.get(0), profileName, trimDifferential, url, srcSD);
           removeStatusExtensions(outcome);
 //          if (outcome.getPath().endsWith("[x]") && outcome.getType().size() == 1 && !outcome.getType().get(0).getCode().equals("*") && !diffMatches.get(0).hasSlicing()) // if the base profile allows multiple types, but the profile only allows one, rename it
 //            outcome.setPath(outcome.getPath().substring(0, outcome.getPath().length()-3)+Utilities.capitalize(outcome.getType().get(0).getCode()));
@@ -1157,7 +1148,7 @@ public class ProfileUtilities extends TranslatingUtilities {
 
             // differential - if the first one in the list has a name, we'll process it. Else we'll treat it as the base definition of the slice.
             if (!diffMatches.get(0).hasSliceName()) {
-              updateFromDefinition(outcome, diffMatches.get(0), profileName, trimDifferential, url, srcSD, destKind);
+              updateFromDefinition(outcome, diffMatches.get(0), profileName, trimDifferential, url, srcSD);
               removeStatusExtensions(outcome);
               if (!outcome.hasContentReference() && !outcome.hasType()) {
                 throw new DefinitionException("not done yet");
@@ -1518,7 +1509,7 @@ public class ProfileUtilities extends TranslatingUtilities {
               if (!outcome.getPath().startsWith(resultPathBase))
                 throw new DefinitionException("Adding wrong path");
               result.getElement().add(outcome);
-              updateFromDefinition(outcome, diffItem, profileName, trimDifferential, url, srcSD, destKind);
+              updateFromDefinition(outcome, diffItem, profileName, trimDifferential, url, srcSD);
               removeStatusExtensions(outcome);
               // --- LM Added this
               diffCursor = differential.getElement().indexOf(diffItem)+1;
@@ -2235,7 +2226,7 @@ public class ProfileUtilities extends TranslatingUtilities {
     return true;
   }
 
-  private void updateFromDefinition(ElementDefinition dest, ElementDefinition source, String pn, boolean trimDifferential, String purl, StructureDefinition srcSD, StructureDefinitionKind destKind) throws DefinitionException, FHIRException {
+  private void updateFromDefinition(ElementDefinition dest, ElementDefinition source, String pn, boolean trimDifferential, String purl, StructureDefinition srcSD) throws DefinitionException, FHIRException {
     source.setUserData(GENERATED_IN_SNAPSHOT, dest);
     // we start with a clone of the base profile ('dest') and we copy from the profile ('source')
     // over the top for anything the source has
